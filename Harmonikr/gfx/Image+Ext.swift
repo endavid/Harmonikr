41cdb9190a1bbe6ee9851ebe4b7d6af2d646686e
David Gavilan <david@metail.co.uk>
2021-07-25 18:40:45 +0100

    Fix image cropping to allow HDR images -- for when importing side-cross cubemaps

--- Harmonikr/gfx/Image+Ext.swift
+++ Harmonikr/gfx/Image+Ext.swift
@@ -9,13 +9,15 @@
 import AppKit
 
 extension NSImage {
-    func trim(rect: CGRect) -> NSImage {
-        // https://stackoverflow.com/a/56622501/1765629
-        let result = NSImage(size: rect.size)
-        result.lockFocus()
-        let destRect = CGRect(origin: .zero, size: result.size)
-        self.draw(in: destRect, from: rect, operation: .copy, fraction: 1.0)
-        result.unlockFocus()
-        return result
+    func cropping(to rect: CGRect) -> NSImage {
+        // https://stackoverflow.com/a/68521084/1765629
+        var imageRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        guard let imageRef = self.cgImage(forProposedRect: &imageRect, context: nil, hints: nil) else {
+            return NSImage(size: rect.size)
+        }
+        guard let crop = imageRef.cropping(to: rect) else {
+            return NSImage(size: rect.size)
+        }
+        return NSImage(cgImage: crop, size: NSZeroSize)
     }
 }
